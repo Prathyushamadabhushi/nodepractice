54050e080afa70420fc137a0e5612ac6e401f11d
Prathyusha <prathyusham0001@gmail.com>
2019-03-19 19:37:52 -0700

    posting data

--- server.js
+++ server.js
@@ -1,16 +1,25 @@
 var http = require('http');
 var url = require('url');
 
-console.log('***** HTTP MODULE *****');
-function startServer(route, handle){
-    function onRequest(request,response){
-        var pathname = url.parse(request.url).pathname;
-        console.log('request received for', pathname);
-        route(handle, pathname, response);
-    }
-    http.createServer(onRequest).listen(8081);
 
-    console.log("Server started on localhost port 8081");
+function startserver(route, handle){
+	function onRequest(request,response){
+		var reviewData = "";
+		var pathname = url.parse(request.url).pathname;
+		console.log("Request received for " + pathname);
+		request.setEncoding("utf8");
+
+		request.addListener("data",function(chunk){
+			reviewData += chunk;
+		});
+
+		request.addListener("end",function(){
+			route(handle,pathname,response,reviewData);
+		})
+		
+
+	}
+	http.createServer(onRequest).listen(8888);
+	console.log("Server started on localhost port 8888");
 }
-
-exports.startServer = startServer;
+exports.startserver = startserver;
